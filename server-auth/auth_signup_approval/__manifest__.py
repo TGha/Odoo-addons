7ff741ca81e6bd49ca54cec197383062694d5059
TGha <rtsiori@gmail.com>
2022-03-06 15:47:23 +0100

    [ADD] 15.0 : add module auth_signup_approval

---
+++ server-auth/auth_signup_approval/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Signup Approval',
+    'version': '1.0',
+    'sequence': 1,
+    'description': """
+    Approved user registrations
+    """,
+    'category': 'Hidden/Tools',
+    'author': 'RAVALISON A. Tsiorimampionina',
+    'website': '',
+    'depends': ['auth_signup'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
